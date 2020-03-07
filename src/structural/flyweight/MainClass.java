cf45402c8a429adfa1e3bfdc9b25c35ff4bdef56
wangxiangjun <adolph_jun@163.com>
2020-03-07 22:42:59 +0800

    添加享元模式

---
+++ src/structural/flyweight/MainClass.java
@@ -0,0 +1,32 @@
+package structural.flyweight;
+
+/**
+ * 享元模式
+ * 意图：运用共享技术有效地支持大量细粒度的对象。
+ * 主要解决：在有大量对象时，有可能会造成内存溢出，我们把其中共同的部分抽象出来，如果有相同的业务请求，直接返回在内存中已有的对象，避免重新创建。
+ *
+ */
+public class MainClass {
+    private static final String colors[] =
+            { "Red", "Green", "Blue", "White", "Black" };
+    public static void main(String[] args) {
+
+        for(int i=0; i < 20; ++i) {
+            Circle circle =
+                    (Circle)ShapeFactory.getCircle(getRandomColor());
+            circle.setX(getRandomX());
+            circle.setY(getRandomY());
+            circle.setRadius(100);
+            circle.draw();
+        }
+    }
+    private static String getRandomColor() {
+        return colors[(int)(Math.random()*colors.length)];
+    }
+    private static int getRandomX() {
+        return (int)(Math.random()*100 );
+    }
+    private static int getRandomY() {
+        return (int)(Math.random()*100);
+    }
+}
